571bb6321f37640218c240d018db9702c48c5086
Lev Dzhepko <levdjepko@gmail.com>
2020-06-09 15:08:55 -0700

    Updated password checker

--- EntryValidator.cs
+++ EntryValidator.cs
@@ -1,31 +1,83 @@
 ﻿using System.Text;
 using System;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace Core
 {
     public static class EntryValidator
     {
-        public static bool CheckPassword(string raw)
+        public static bool CheckPassword(string pass)
         {
-            bool stringPasses = false;
-            //check password length
-            if (raw.Length < 5 || raw.Length > 12)
+            //checks password length
+            if (pass.Length < 5 || pass.Length > 12)
             {
-                stringPasses = false;
-                return stringPasses;
+                return false;
             }
+            
+            //Does not contain special characters
+            string specialCharacters = @"%!@#$%^&*()?/>.<,:;'\|}]{[_~`+=-" + "\"";
+            char[] specialCharactersArray = specialCharacters.ToCharArray();
+            foreach (char c in specialCharactersArray)
+            {
+                if (pass.Contains(c))
+                    return false;
+            }
+
+            //No white space
+            if (pass.Contains(" "))
+            {
+                return false;
+            }
+
             //check if password consists of letters and/or numbers with at least 1 of each
             Regex lettersAndNumbersOnly = new Regex(@"[a-zA-Z0-9]", RegexOptions.IgnoreCase);
-            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            string digits = "0123456789";
-            if (lettersAndNumbersOnly.IsMatch(raw) && raw.Contains(letters) && raw.Contains(digits)){
-                stringPasses = true;
+            string letters = @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            string digits = @"0123456789";
+            char[] letterChars = letters.ToCharArray();
+            char[] digitsChars = digits.ToCharArray();
+
+            int checkedLetters = 0;
+            foreach (char c in letterChars)
+            {
+                if (pass.Contains(c))
+                {
+                    checkedLetters++;
+                }
             }
+            foreach (char c in digitsChars)
+            {
+                if (pass.Contains(c))
+                {
+                    checkedLetters++;
+                }
+            }
+            if (! (checkedLetters == pass.Length))
+            {
+                return false;
+            }
+            
+
+            //two consecutive characters are the same
+            for (int i = 0; i < pass.Length - 1; i++)
+            {
+                if (pass[i] == pass[i + 1])
+                    return false;
+            }
+
             //check if there's any sequence of characters that is repeated in the password
+            //TODO not done yet...
+            /*
+            List<string> allCombinations = new List<string>();
+            for (int i = 0; i < pass.Length / 2; i++)
+            {
+                for (int j = 1; j < pass.Length; j++)
+                {
+                    allCombinations.Add(pass.Substring(i, j - i));
+                }
+            }*/
 
-
-            return stringPasses;
+            return true;
         }
     }
 }
--- MainActivity.cs
+++ MainActivity.cs
@@ -12,9 +12,39 @@ namespace username_Keeper
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            // Get our UI controls from the loaded layout
+            EditText    username = FindViewById<EditText>(Resource.Id.nameText);
+            EditText    password = FindViewById<EditText>(Resource.Id.passwordText);
+            Button      addButton = FindViewById<Button>(Resource.Id.AddButton);
+            Button      loadAllButton = FindViewById<Button>(Resource.Id.LoadAll);
+
+
+            addButton.Click += (sender, e) =>
+            {
+                // Check if the password is correct and username is not empty
+                if (Core.EntryValidator.CheckPassword(password.Text) && username.Text.Length > 0)
+                {
+                    FindViewById<TextView>(Resource.Id.addedText).Text = $"Your username is {username.Text} ," +
+                    $" password is {password.Text}";
+                }
+                else
+                {
+                    Android.App.AlertDialog.Builder dialog = new Android.App.AlertDialog.Builder(this);
+                    Android.App.AlertDialog alert = dialog.Create();
+                    alert.SetTitle("Password not complaint");
+                    alert.SetMessage("Your username should be not empty. \n Password must only contain letters and digits, be between 5 and 12 letters long, have at least" +
+                        " 1 letter and 1 number, and not have recurring substrings (like abcabc");
+                    alert.SetButton("OK", (c, ev) =>
+                    {
+                        // no action required
+                    });
+                    alert.Show();
+                }
+            };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
